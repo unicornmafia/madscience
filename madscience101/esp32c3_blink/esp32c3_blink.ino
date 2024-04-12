4348820543014d5a34bedd493463d98951a8b94a
thomas <thomas@marshbot.com>
2024-04-12 00:02:18 -0700

    updating structure

--- madscience101/esp32c3_blink/esp32c3_blink.ino
+++ madscience101/esp32c3_blink/esp32c3_blink.ino
@@ -9,8 +9,8 @@ void setup() {
 
 // the loop function runs over and over again forever
 void loop() {
-  digitalWrite(LED_BUILTIN, HIGH);  // turn the LED on (HIGH is the voltage level)
+  digitalWrite(ESP_C3_LED_BUILTIN_PIN, HIGH);  // turn the LED on (HIGH is the voltage level)
   delay(1000);                      // wait for a second
-  digitalWrite(LED_BUILTIN, LOW);   // turn the LED off by making the voltage LOW
+  digitalWrite(ESP_C3_LED_BUILTIN_PIN, LOW);   // turn the LED off by making the voltage LOW
   delay(1000);                      // wait for a second
 }
